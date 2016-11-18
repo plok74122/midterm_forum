c735315bf6a497402253d68792321e830aa69737
madchun <sccheungad@gmail.com>
2016-11-18 20:38:14 +0800

    index added, add table boostrap on index page

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,29 @@
 class RequestsController < ApplicationController
+  before_action :set_request, :only => [:show, :edit, :update, :destroy]
+
+  def index
+    @requests = Request.page(params[:page]).per(10)
+    if params[:id]
+      @request = Request.find(params[:id])
+    else
+      @request = Request.new
+    end
+  end
+
+  def show
+  end
+
+  
+
+  private
+  def set_request
+    @request = Request.find(params[:id])
+  end
+
+  def request_params
+    params.require(:request).permit(:title, :content)
+  end
+
+
+
 end
